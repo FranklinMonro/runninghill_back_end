cda375c61aecc9687e67e1ef0ca140745dc36fa7
FranklinMonro <fbg2468@gmail.com>
2023-06-26 15:33:19 +0000

    #add eslint

--- server/app.ts
+++ server/app.ts
@@ -9,65 +9,63 @@ import SentenceRouter from '../routes/sentence/sentenceRoutes';
 import WordsApiRouter from '../routes/wordsAPI/worsApiRoutes';
 
 class App {
-    public httpServer = express();
+  public httpServer = express();
 
-    constructor() {
+  constructor() {
+    this.httpServer.use(json({ limit: '500mb' }));
 
-        this.httpServer.use(json({ limit: '500mb' }));
+    this.httpServer.use(urlencoded({ extended: true, limit: '500mb' }));
 
-        this.httpServer.use(urlencoded({ extended: true, limit: '500mb' }));
+    this.httpServer.use((req: Request, res: Response, next: NextFunction) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Authorization, X-CSRFToken',
+      );
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, OPTIONS, DELETE');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      next();
+    });
 
-        this.httpServer.use((req: Request, res: Response, next: NextFunction) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Authorization, X-CSRFToken',
-            );
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, OPTIONS, DELETE');
-            res.header('Access-Control-Allow-Credentials', 'true');
-            next();
-        });
+    this.httpServer.set('trust proxy', true);
 
-        this.httpServer.set('trust proxy', true);
+    this.httpServer.get('/', (req: Request, res: Response) => {
+      console.log('Welcome to Runninghill API');
+      res.send('Welcome to Runninghill API');
+    });
 
-        this.httpServer.get('/', (req: Request, res: Response) => {
-            console.log('Welcome to Runninghill API');
-            res.send('Welcome to Runninghill API');
-        });
+    // this.httpServer.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
-        // this.httpServer.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
-    
-        this.httpServer.use('/runninghillapi/senttenceroutes', SentenceRouter);
+    this.httpServer.use('/runninghillapi/senttenceroutes', SentenceRouter);
 
-        this.httpServer.use('/runninghillapi/wordsapi', WordsApiRouter);
+    this.httpServer.use('/runninghillapi/wordsapi', WordsApiRouter);
 
-        this.httpServer.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-            console.log(`error in url ${req.originalUrl} - error: ${err}`);
-            log.error(`error in url ${req.originalUrl} - error: ${err.message}`);
-            const errormessage = createErrorMessage(err);
-            errorLog.log('error', `Error in File: ${errormessage.fileName} on line: ${errormessage.line}, error: ${err}`);
-            const statusCode = errormessage.statusCode || 403; // <- Look here
-            res.status(statusCode).json({
-                detail: err.message,
-            });
-            next();
-        });
+    this.httpServer.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+      console.log(`error in url ${req.originalUrl} - error: ${err}`);
+      log.error(`error in url ${req.originalUrl} - error: ${err.message}`);
+      const errormessage = createErrorMessage(err);
+      errorLog.log('error', `Error in File: ${errormessage.fileName} on line: ${errormessage.line}, error: ${err}`);
+      const statusCode = errormessage.statusCode || 403; // <- Look here
+      res.status(statusCode).json({
+        detail: err.message,
+      });
+      next();
+    });
 
-        process.once('uncaughtException', (err: Error) => {
-            console.log(err.name, err.message);
-            console.log('UNCAUGHT EXCEPTION!  Shutting down...');
-            log.log('error', `uncaughtException - Error name: ${err.name}, Error message: ${err.message}, UNCAUGHT EXCEPTION!  Shutting down...`);
-            process.exit(1);
-        });
+    process.once('uncaughtException', (err: Error) => {
+      console.log(err.name, err.message);
+      console.log('UNCAUGHT EXCEPTION!  Shutting down...');
+      log.log('error', `uncaughtException - Error name: ${err.name}, Error message: ${err.message}, UNCAUGHT EXCEPTION!  Shutting down...`);
+      process.exit(1);
+    });
 
-        process.once('unhandledRejection', (reason: Error) => {
-            console.log(reason.name, reason.message);
-            console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-            log.log('error', `uncaughtException - Reason name: ${reason.name}, Reason message: ${reason.message}, UNHANDLED REJECTION!  Shutting down...`);
-            process.exit(1);
-        });
-
-    }
+    process.once('unhandledRejection', (reason: Error) => {
+      console.log(reason.name, reason.message);
+      console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+      log.log('error', `uncaughtException - Reason name: ${reason.name}, Reason message: ${reason.message}, UNHANDLED REJECTION!  Shutting down...`);
+      process.exit(1);
+    });
+  }
 }
 
 export default new App().httpServer;
--- server/config.ts
+++ server/config.ts
@@ -14,41 +14,41 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const PG_CONNECTION = {
-    host: process.env.POSTGRES_HOST, // host of db container
-    port: 5432, // 5432 is the default;
-    database: process.env.POSTGRES_DB, // database name
-    user: process.env.POSTGRES_USER, // database user name
-    password: process.env.POSTGRES_PWD, // database password
+  host: process.env.POSTGRES_HOST, // host of db container
+  port: 5432, // 5432 is the default;
+  database: process.env.POSTGRES_DB, // database name
+  user: process.env.POSTGRES_USER, // database user name
+  password: process.env.POSTGRES_PWD, // database password
 };
 
 const SEQUILIZE_NEW = new Sequelize({
-    host: PG_CONNECTION.host,
-    dialect: 'postgres',
-    port: PG_CONNECTION.port,
-    database: PG_CONNECTION.database,
-    username: PG_CONNECTION.user,
-    password: PG_CONNECTION.password,
-    benchmark: true,
-    logging: false, // console.log, // false,
+  host: PG_CONNECTION.host,
+  dialect: 'postgres',
+  port: PG_CONNECTION.port,
+  database: PG_CONNECTION.database,
+  username: PG_CONNECTION.user,
+  password: PG_CONNECTION.password,
+  benchmark: true,
+  logging: false, // console.log, // false,
 });
 
 const POOL_PG = new Pool(PG_CONNECTION);
 
 const connectToDB = async () => {
-    try {
-        await POOL_PG.connect();
-        SEQUILIZE_NEW.authenticate();
-        console.log('PostgresSQL connection made');
-        } catch (err) {
-        log.log('error', `Error in establish connection with DB error: ${err}`);
-        console.log(err);
-    }
+  try {
+    await POOL_PG.connect();
+    SEQUILIZE_NEW.authenticate();
+    console.log('PostgresSQL connection made');
+  } catch (err) {
+    log.log('error', `Error in establish connection with DB error: ${err}`);
+    console.log(err);
+  }
 };
 
 connectToDB();
 
 export {
-    PORT,
-    HOST,
-    SEQUILIZE_NEW,
+  PORT,
+  HOST,
+  SEQUILIZE_NEW,
 };
