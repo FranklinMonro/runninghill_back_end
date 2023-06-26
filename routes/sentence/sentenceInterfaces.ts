b594f40ef6f35c409afb8637ced866a87d732979
FranklinMonro <fbg2468@gmail.com>
2023-06-26 15:32:03 +0000

    #add eslint

--- routes/sentence/sentenceInterfaces.ts
+++ routes/sentence/sentenceInterfaces.ts
@@ -1,14 +1,14 @@
 interface WordTypes {
-    typeID: string;
-    wordType?: string;
+  typeID: string;
+  wordType?: string;
 }
 
 interface SentenceTypes {
-    senteceID: string;
-    sentence?: string;
+  senteceID: string;
+  sentence?: string;
 }
 
 export {
-    WordTypes,
-    SentenceTypes,
+  WordTypes,
+  SentenceTypes,
 };
